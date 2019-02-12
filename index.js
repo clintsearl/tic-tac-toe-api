1832a76ccb8ef2e7722a49050087520ae4e1dff8
Tyler Garlick <tjgarlick@gmail.com>
2019-02-12 12:27:45 -0700

    Move consts
    -- PORT is created at the top

--- index.js
+++ index.js
@@ -1,61 +1,50 @@
 const {
-  json
+  json,
 } = require('micro')
 const micro = require('micro')
 
+const PORT = process.env.PORT || 3000
+
+const checkForNullValues = (...values) =>
+  values.some(value => value !== null)
+
 const checkValues = (...values) =>
-  values.every((value) => values[0] === value)
+  values.every((value) => values[0] === value) &&
+  checkForNullValues(values)
 
+const isValid = board =>
+  board.length > 5 &&
+  board.length <= 9
 
-// function checkRows(board) {
+const checkRows = board =>
+  checkValues(board[0], board[1], board[2]) ||
+  checkValues(board[3], board[4], board[5]) ||
+  checkValues(board[6], board[7], board[8])
 
+const checkColumns = board =>
+  checkValues(board[0], board[3], board[6]) ||
+  checkValues(board[1], board[4], board[7]) ||
+  checkValues(board[2], board[5], board[8])
 
-//   if (board[0] === board[1] === board[2]) return true
-//   if (board[3] === board[4] === board[5]) return true
-//   if (board[6] === board[7] === board[8]) return true
+const checkDiagonals = board =>
+  checkValues(board[0], board[4], board[8]) ||
+  checkValues(board[2], board[4], board[6])
 
-//   return false
-
-// }
-
-const checkRows = (board = ['', '']) => {
-  // if (board[0] === board[1] === board[2]) return true
-  // if (board[3] === board[4] === board[5]) return true
-  // if (board[6] === board[7] === board[8]) return true
-
-  // return false
-
-
-  return checkValues(board[0], board[1], board[2]) ||
-    checkValues(board[3], board[4], board[5]) ||
-    checkValues(board[6], board[7], board[8])
-}
-
-const checkColumns = (board) => {
-  return checkValues(board[0], board[3], board[6]) ||
-    checkValues(board[1], board[4], board[7]) ||
-    checkValues(board[2], board[5], board[8])
-}
-
-const checkDiagonals = (board) => {
-  return checkValues(board[0], board[4], board[8]) ||
-    checkValues(board[2], board[4], board[6])
-}
-
-const hasWon = (board) =>
-  checkRows(board) ||
-  checkColumns(board) ||
-  checkDiagonals(board)
-
+const hasWon = (board = []) =>
+  isValid(board) && (
+    checkRows(board) ||
+    checkColumns(board) ||
+    checkDiagonals(board)
+  )
 
 const format = (board) =>
   typeof (board) === 'string' ?
-  board.split(' ') :
-  board.flat()
+    board.split(' ') :
+    board.flat()
 
 const server = micro(async (req, res) => {
   const {
-    input
+    input,
   } = await json(req)
   
   console.log(input)
@@ -63,10 +52,8 @@ const server = micro(async (req, res) => {
   console.log(formatted)
   
   return {
-    won: hasWon(formatted)
+    won: hasWon(formatted),
   }
 })
-
-const PORT = process.env.PORT || 3000
 
 server.listen(PORT)
